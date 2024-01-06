3b96f0b3111f81efb9d844e4e299ddb20844e2f2
RakibR7 <G00408644@ATU.IE>
2024-01-06 19:40:22 +0000

    base

---
+++ src/test/java/atu/ie/transactionserviceproject/TransactionServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.transactionserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
